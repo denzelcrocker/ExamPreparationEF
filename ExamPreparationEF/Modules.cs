780d50c9ecc7617c81ec18b8627ae2b16894abc4
Дениска Зеленцов <zelentsov36@gmail.com>
2023-04-10 17:17:38 +0300

    Добавьте файлы проекта.

---
+++ ExamPreparationEF/Modules.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPreparationEF
+{
+    internal class Modules
+    {
+        public static string OpenDialog()
+        {
+            string path = null;
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                path = openFileDialog.FileName;
+            }
+            return path;
+        }
+        public static void WriteChildrens(List<Children> childrens, string path)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    foreach (Children item in childrens)
+                    {
+                        string line = $"{item.Name},{item.Birthday},{item.Gender}";
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ ExamPreparationEF/Pages/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamPreparationEF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public List<Children> childrens;
+        public MainPage()
+        {
+            InitializeComponent();
+            ExapPreparationEfContext db = new ExapPreparationEfContext();
+            childrens = db.Childrens.ToList();
+            AllChildrens.ItemsSource = childrens;
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Modules.OpenDialog();
+            Modules.WriteChildrens(childrens, path);
+        }
+    }
+}
